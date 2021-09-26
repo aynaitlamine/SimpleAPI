edad8ffd4cfe02a7d4d615e743ce2f1debf2a56f
Ayoub Nait Lamine <naitlamine.dev@gmail.com>
2021-09-26 09:50:28 +0100

    Build project

---
+++ SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SimpleAPI.API.Controllers;
+using SimpleAPI.API.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+
+        
+    }
+}
