f7de2f593f07d35cf8c8d75d4a5e835386c427fb
Ayoub Nait Lamine <naitlamine.dev@gmail.com>
2021-09-26 09:56:20 +0100

    Add integration test

--- SimpleAPI.Test/UnitTest1.cs
+++ SimpleAPI.Test/UnitTest1.cs
@@ -17,6 +17,36 @@ namespace SimpleAPI.Test
             Assert.True(1 == 1);
         }
 
-        
+        [Fact]
+        public async Task CustomerIntegrationTest()
+        {
+            // Create DB Context
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            var context = new CustomerContext(optionsBuilder.Options);
+
+            // Just to make sure: Delete all existing customers in the DB
+
+            
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            // Create Controller
+
+            var controller = new CustomersController(context);
+
+            // Add customer
+            await controller.Add(new Customer() { Name = "Rachid" });
+
+            var result = (await controller.GetAll()).ToArray();
+            Assert.Single(result);
+            Assert.Equal("Rachid", result[0].Name);
+        }
     }
 }
